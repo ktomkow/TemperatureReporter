c10196dd03eef1e7e5e8402fe165c5a451ecb7ce
ktomkow <krzysztof.tomkow@gmail.com>
2021-09-05 19:50:34 +0200

    Fake measurer again

---
+++ src/TemperatureReporter.App/FakeMeasurer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TemperatureReporter.App
+{
+    public class FakeMeasurer : IMeasurer
+    {
+        private readonly Random random;
+
+        public FakeMeasurer()
+        {
+            this.random = new Random();
+        }
+
+        public Measurement Get()
+        {
+            int randInt = this.random.Next(40, 80);
+
+            return new Measurement(randInt + this.random.NextDouble());
+        }
+    }
+}
